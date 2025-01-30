a66ed7dae5338e6a8767a58baf0b3573ffe8e959
MoniRanj <ranj.monica@gmail.com>
2025-01-30 17:04:34 +0530

    first push

---
+++ seleniumProject/src/test/java/pack2/orgTest.java
@@ -0,0 +1,16 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createorgTest()
+	{
+		System.out.println("execute createorgTest");
+	}
+	@Test
+	public void modifyorgTest()
+	{
+		System.out.println("execute modifyorgTest");
+	}
+}
